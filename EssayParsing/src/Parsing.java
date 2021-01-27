403d4b5a72ff6043b449c53adb25eb8797645c93
DenysVodzyk <vodzyk@gmail.com>
2021-01-26 20:34:59 -0500

    Added more functionality

--- EssayParsing/src/Parsing.java
+++ EssayParsing/src/Parsing.java
@@ -2,27 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Parsing {
 
-    public static void main(String[] args) {
-        String in = "C:\\Users\\vodzy\\IdeaProjects\\Go2ItBootCamp_FileReader\\EssayParsing.csv";
-        Parsing parsing = new Parsing();
-        System.out.println(parsing.readFromFile(in));
-
-    }
-
     public String readFromFile(String path) {
-        String input = "";
-        String neww = "";
+        String lineReader;
+        String output = "";
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
-
-            while ((input = reader.readLine()) != null) {
-                neww += input;
+            while ((lineReader = reader.readLine()) != null) {
+                output += lineReader;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return neww;
+        return output;
     }
+
+
+    public String[] splitStringIntoArray(String input) {
+        return input.split("\\s+");
+    }
+
+
+    public int determineNumberOfWords(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+        String[] words = splitStringIntoArray(input);
+        return words.length;
+    }
+
+    public int determineNumberOfSentences(String input) {
+        int numberOfSentences = 0;
+        if (input.isEmpty()) {
+            return numberOfSentences;
+        }
+        String[] words = splitStringIntoArray(input);
+
+        for (String word : words) {
+            if (word.charAt(word.length() - 1) == '!' || word.charAt(word.length() - 1) == '.' || word.charAt(word.length() - 1) == '?') {
+                numberOfSentences++;
+            }
+        }
+        return numberOfSentences;
+    }
+
+
 }
